3f3a2640a493e44e6a3d31568c85a54bb2804c82
naemcivic <naemcivic@gmail.com>
2016-04-23 15:16:37 -0400

    create actions for Redux in action.js

---
+++ action.js
@@ -0,0 +1,203 @@
+//user
+{
+  type: AUTH_USER,
+  uid,
+}
+
+{
+  type: UNAUTH_USER,
+}
+
+{
+  type: FETCHING_USER,
+}
+
+{
+  type:  FETCHING_USER_FAILURE,
+  error: 'error fetching user',
+}
+
+{
+  type: FETCHING_USER_SUCCESS,
+  uid,
+  user,
+  timestamp,
+}
+
+
+// Ducks
+{
+  type: FETCHING_DUCK,
+}
+
+{
+  type: FETCHING_DUCK_ERROR,
+  error: 'Error fetching duck',
+}
+
+{
+  type: FETCHING_DUCK_SUCCESS,
+  duck,
+}
+
+{
+  type: REMOVE_FETCHING,
+}
+
+{
+  type: ADD_DUCK,
+  duck,
+}
+
+{
+  type: ADD_MULTIPLE_DUCKS,
+  ducks,
+}
+
+
+//feed
+{
+  type: SETTING_FEED_LISTENER,
+}
+
+{
+  type: SETTING_FEED_LISTENER_ERROR,
+  error: 'Error fetching feeds.',
+}
+
+{
+  type: SETTING_FEED_LISTENER_SUCCESS,
+  duckIds,
+}
+
+{
+  type: ADD_NEW_DUCK_ID_TO_FEED,
+  duckId,
+}
+
+{
+  type: RESET_NEW_DUCKS_AVAILABLE,
+}
+
+// listeners
+{
+  type: ADD_LISTENER,
+  listenerId,
+}
+
+// Modal
+
+{
+  type: OPEN_MODAL,
+}
+
+{
+  type: CLOSE_MODAL,
+}
+
+{
+  type: UPDATE_DUCK_TEXT,
+  newDuckText,
+}
+
+// replies
+{
+  type: FETCHING_REPLIES,
+}
+
+{
+  type: FETCHING_REPLIES_ERROR,
+  error: 'Error fetching replies',
+}
+
+{
+  type: FETCHING_REPLIES_SUCCESS,
+  replies,
+  duckId,
+  lastUpdated: Date.new(),
+}
+
+
+{
+  type: ADD_REPLY,
+  duckID,
+  reply,
+}
+
+{
+  type: ADD_REPLY_ERROR,
+  error: 'Error adding reply',
+}
+
+{
+  type: REMOVE_REPLY,
+  replyId,
+}
+
+// likeCount
+
+{
+  type: FETCHING_COUNT,
+}
+
+{
+  type: FETCHING_COUNT_ERROR,
+  error: 'Error fetching duck\'s like count',
+}
+
+{
+  type: FETCHING_COUNT_SUCCESS,
+  duckId,
+  count,
+}
+
+// Users Ducks
+
+{
+  type: FETCHING_USERS_DUCKS,
+  uid,
+}
+
+{
+  type: FETCHING_USERS_DUCKS_ERROR,
+  error: 'Error fetching Users ducks'
+}
+
+{
+  type: FETCHING_USERS_DUCKS_SUCCESS,
+  uid,
+  duckIds,
+  lastUpdated,
+}
+
+{
+  type: ADD_SINGLE_USERS_DUCK,
+  uid,
+  duckIds,
+}
+
+// Users Likes
+
+{
+  type: FETCHING_LIKES,
+}
+
+{
+  type: FETCHING_LIKE_ERROR,
+  error: 'Error fetching likes'
+}
+
+{
+  type: FETCHING_LIKE_SUCCESS,
+  likes,
+}
+
+{
+  type: ADD_A_LIKE,
+  duckId,
+}
+
+{
+  type: REMOVE_LIKE,
+  duckId,
+}
