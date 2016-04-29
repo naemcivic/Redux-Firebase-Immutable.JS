e457f12fc313da39d5bb56f493ba71645731de15
naemcivic <naemcivic@gmail.com>
2016-04-29 19:17:03 -0400

    setup webpack, babel and render first component

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const htmlwebpackPluginConfig = new htmlWebpackPlugin ({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
-  
+  entry: [
+    './app/index.js',
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js',
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, loader: 'style-loader!css-loader'}
+    ]
+  },
+  plugins: [htmlwebpackPluginConfig]
 }
