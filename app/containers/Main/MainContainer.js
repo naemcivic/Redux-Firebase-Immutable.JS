0b7803633b140488e3ba2c24af8c5649e6a5fae8
naemcivic <naemcivic@gmail.com>
2016-05-04 14:05:22 -0400

    create navigation component

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { container, innerContainer } from './styles.css'
-
+import { Navigation } from 'components'
 class MainContainer extends Component {
   render () {
     return (
       <div className={container}>
+        <Navigation />
         <div className={innerContainer}>
           {this.props.children}
         </div>
